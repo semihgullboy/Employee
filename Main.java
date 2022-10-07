03d392aab5880bdec67829d3899f34ce6f250d16
semihgullboy <smhglby17@gmail.com>
2022-10-07 18:43:37 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Employee kullanici1= new Employee("Semih",2000,45,1985);
+		 kullanici1.toPrint();
+
+	}
+
+}
